b8676f545fe37002ea41901e47f57c078d55be2e
prarthanaprarthana664-creator <prarthanaprarthana664@gmail.com>
2026-03-20 22:02:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,18 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Server Performance Monitoring Dashboard")
+st.title("Server Monitoring Dashboard")
 
-df = pd.read_excel("server_data.xlsx")
+try:
+    df = pd.read_excel("server_data.xlsx")
 
-st.subheader("Dataset Preview")
-st.write(df.head())
+    st.write(df.head())
 
-if "CPU_Utilization (%)" in df.columns:
-    st.subheader("CPU Utilization")
-    st.line_chart(df["CPU_Utilization (%)"])
+    if "CPU_Utilization (%)" in df.columns:
+        st.line_chart(df["CPU_Utilization (%)"])
 
-if "Memory_Usage (%)" in df.columns:
-    st.subheader("Memory Usage")
-    st.line_chart(df["Memory_Usage (%)"])
+    if "Memory_Usage (%)" in df.columns:
+        st.line_chart(df["Memory_Usage (%)"])
+
+except Exception as e:
+    st.error(e)
